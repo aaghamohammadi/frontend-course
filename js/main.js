31b254d5f650d2cd5707befb9f391ec537c0b2e8
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2022-07-30 07:13:22 +0430

    js data types

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log(2 + 3);
+//  This is a comment
+/* 
+1. comments
+2. comments
+*/
+console.log(typeof 2);
+console.log(typeof 3.5);
+console.log(typeof "Alireza");
+console.log(3 === 3);
+console.log(3 !== 5);
+console.log(3 !== 3);
+console.log(typeof true);
+console.log(typeof false);
+let firstName;
+console.log(firstName);

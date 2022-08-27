f247c32d652d25d4edaa9834ab182807d6fb36df
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2022-08-27 08:38:36 +0430

    add intro to DOM

--- js/main.js
+++ js/main.js
@@ -1,28 +1,8 @@
 "use strict";
 
-for (let i = 0; i < 5; i++) {
-  console.log(i);
-}
+const header = document.querySelector(".header__ul");
 
-const cities = ["Tehran", "Shiraz", "Esfahan", "Tabriz"];
+console.log(header);
 
-for (let i = 0; i < cities.length; i++) {
-  console.log(cities[i]);
-}
-
-for (let i = cities.length - 1; i >= 0; i--) {
-  console.log(cities[i]);
-}
-
-const arr = [5, 1, 12, 19, -23];
-
-let i = 0;
-let min = arr[i];
-while (i < arr.length) {
-  i++;
-  if (min < arr[i]) {
-    min = arr[i];
-  }
-}
-
-console.log(min);
+const divs = document.querySelectorAll("div");
+console.log(divs);

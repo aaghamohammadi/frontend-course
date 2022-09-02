cbf274c4ac988183d3fb8030a48d861ba71a0bec
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2022-09-02 11:41:23 +0430

    add keyboard control

--- Project001/js/main.js
+++ Project001/js/main.js
@@ -5,3 +5,9 @@ const headerNav = document.querySelector(".header__ul");
 mobileNav.addEventListener("click", () => {
   headerNav.classList.toggle("active");
 });
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "u" && event.ctrlKey) {
+    event.preventDefault();
+  }
+});
